397d38f1f2d997d9901ded261633c7258f1cc939
Jared Shetler <jshetler7@gmail.com>
2022-08-19 17:39:33 -0500

    Version 1.0.0 Completed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,50 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { MdOutlineWbSunny } from 'react-icons/md';
+import { RiMoonLine } from 'react-icons/ri';
 
 const Navbar = () => {
+
+    const [isDark, setIsDark] = useState<boolean>(false);
+
+    useEffect(() => {
+      if (isDark) {
+        document.body.classList.add('dark');
+      } else {
+        document.body.classList.remove('dark');
+      }
+    }, [isDark]);
+
+    const handleTheme = () => {
+        setIsDark(!isDark);
+    }
+    
+    const styleDark = { color: "white", fontSize: "1.5em" }
+    const styleLight = { color: "black", fontSize: "1.5em" }
+
     return(
-        <div className="grid-cols-4 flex flex-wrap bg-slate-400 pb-5 justify-center gap-x-10">
+        <div className="grid-cols-4 flex pb-5 w-auto md:w-full justify-between ml-2 mr-2 md:m-0 md:justify-center md:gap-x-10 pt-5" id="navbar">
             <Link href={'/'}>
-                <a className="flex text-black text-2xl">Home</a>
-            </Link>
-            <Link href={'/about'}>
-                <a className="flex text-black text-2xl">About</a>
+                <a className="flex text-2xl ml-2 md:m-0 dark:text-white hover:font-semibold hover:underline hover:underline-offset-8 hover:decoration-blue-300">
+                    About
+                </a>
             </Link>
             <Link href={'/portfolio'}>
-                <a className="flex text-black text-2xl">Portfolio</a>
+                <a className="flex text-2xl dark:text-white hover:font-semibold hover:underline hover:underline-offset-8 hover:decoration-blue-300">
+                    Portfolio
+                    </a>
             </Link>
             <Link href={'/resume'}>
-                <a className="flex text-black text-2xl">Resume</a>
+                <a className="flex text-2xl mr-2 md:m-0 dark:text-white hover:font-semibold hover:underline hover:underline-offset-8 hover:decoration-blue-300">
+                    Resume
+                    </a>
             </Link>
+            <button className="flex-row place-self-center justify-end dark:hidden" id="toggleDark" onClick={handleTheme}>
+                <RiMoonLine size={20} style={styleLight} />
+            </button>
+            <button className="flex-row place-self-center justify-end hidden dark:inline-block" onClick={handleTheme}>
+                <MdOutlineWbSunny size={20} style={styleDark} />
+            </button>
         </div>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import type { AppProps } from 'next/app'
 import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log("%cIf you like what you see, I'd love to work with you!", 'background: linear-gradient(153deg,#8fdceb,#142e64); padding: 2em; font-size: 2em; font-weight: bold; color: white');
+  console.log("%cEmail me >>> jshetler7@gmail.com", 'background: linear-gradient(153deg,#8fdceb,#142e64); padding: 1em; font-size: 2em; font-weight: bold; color: white');
 
   return <>
-    <Navbar />
-    <Component {...pageProps} />
+    <div className='bg-slate-100 dark:bg-gray-900'>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
   </>
 
 }
